77cd1a883abd86c14b503e0eca3ec4bf35013233
Luis Gonzalez <luisgnzhsz@gmail.com>
2022-11-28 19:05:42 -0600

    style: added images

--- Adminstracion de Ventas/sources/tattooists.py
+++ Adminstracion de Ventas/sources/tattooists.py
@@ -15,6 +15,10 @@ class Window(Frame):
         self.apellido_p = StringVar
         self.idStyle = StringVar
         self.idArea = StringVar
+        self.search = StringVar()
+        self.searchUpdate = StringVar()
+        self.idTatuadores = StringVar()
+
 
         self.data_base = Data_logging()
 
@@ -40,6 +44,68 @@ class Window(Frame):
 
         self.widgets()
 
+    def homeScreen(self):
+        self.pages.select([self.frameOne])
+    
+    def dataScreen(self):
+        self.pages.select([self.frameTwo])
+        self.frameTwo.columnconfigure(0, weight=1)
+        self.frameTwo.columnconfigure(1, weight=1)
+        self.frameTwo.columnconfigure(2, weight=1)
+        self.frameTwo.columnconfigure(0, weight=1)
+        self.frameTwo.columnconfigure(0, weight=1)
         
+    def screenWrite(self):
+        self.pages.select([self.frameThree])
+        self.frameThree.columnconfigure(0, weight=1)
+        self.frameThree.columnconfigure(1, weght=1)
+        
+    def refreshScreen(self, frameFour):
+        self.pages.select([self.frameFour])
+        self.frameFour.columnconfigure(0, weight=1)
+        self.frameFour.columnconfigure(1, weight=1)
+    
+    def searchScreen(self, frameFive):
+        self.pages.select([self.frameFive])
+        self.frameFive.columnconfigure(0, weight=1)
+        self.frameFive.columnconfigure(1, weight=1)
+        self.frameFive.columnconfigure(2, weight=1)
+        self.frameFive.rowconfigure(2, weight=1)
+        self.frameTableTwo.columnconfigure(0, weight=1)
+        self.frameTableTwo.rowconfigure(0, weight=1)
 
-
+    def lateralMenu(self):
+        if self.menu is True:
+            for i in range(50,170,10):
+                self.frame_menu.config(width= i)
+                self.frame_begin.config(width= i)
+                self.frame_menu.update()
+                click_begin = self.bt_close.grid_forget()
+                if click_begin is None:
+                    self.bt_begin.grid(column=0, row=0, padx=10, pady=10)
+                    self.bt_begin.grid_propagate(0)
+                    self.bt.begin.config(width= i)
+                    self.homeScreen()
+            self.menu = False
+        else:
+            for i in range(50,170,10):
+                self.frame_menu.config(width= i)
+                self.frame_begin.config(width= i)
+                self.frame_menu.update()
+                click_begin = self.bt_begin.grid_forget()
+                if click_begin is None:
+                    self.frame_menu.grid_propagate(0)
+                    self.bt_close.grid(column=0, row=0, padx=10, pady=10)
+                    self.bt_close.grid_propagate(0)
+                    self.bt.close.config(width= i)
+                    self.homeScreen()
+            self.menu = True
+    
+    def wodgets(self):
+        self.imageBegin = PhotoImage(file='home.png')
+        self.imageMenu = PhotoImage(file='menu.png')
+        self.imageData = PhotoImage(file='datos.png')
+        self.imagenRegister = PhotoImage(file='editar.png')
+        self.imageUpdate = PhotoImage(file='actualizar.png')
+        self.imageSearch = PhotoImage(file='buscar.png')
+        
